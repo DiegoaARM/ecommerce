d7941d4041d7f61b6dadee0b1eab639a6daad41d
DiegoaARM <juandiegoduquejimenez@gmail.com>
2025-07-25 14:12:34 -0500

    Commit #1: Se creó el product-service

---
+++ product-service/src/main/java/com/techie/microservices/product/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.techie.microservices.product.service;
+
+import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.dto.ProductResponse;
+import com.techie.microservices.product.model.Product;
+import com.techie.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // Method to create a product
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        product = productRepository.save(product);
+        System.out.println("Product created");
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    // Additional methods for product management can be added here
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+
+}
